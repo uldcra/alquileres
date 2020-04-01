1e25ba613487a762960efa890618c65e70031ab3
uldcra <uldcra@gmail.com>
2020-04-01 20:51:41 +0200

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,29 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { HomeComponent } from './pages/home/home.component';
+import { RecomendacionesComponent } from './components/recomendaciones/recomendaciones.component';
+
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    FooterComponent,
+    HomeComponent,
+    RecomendacionesComponent
   ],
   imports: [
     BrowserModule,
+    FormsModule, 
+    ReactiveFormsModule, 
+    HttpClientModule,
     AppRoutingModule
   ],
   providers: [],
---
+++ src/app/pages/home/home.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent implements OnInit {
+
+  public price:any = '';
+  public rooms:any = '';
+  public bathrooms:any = '';
+  public squareMeters:any = '';
+  public property:any = '';
+  public location:any = '';
+  public type:any = '';
+  public id:any = '';
+  public key:any = '';
+  public value:any = '';
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
